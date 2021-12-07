124e77c62bd4e05bd4c7f94c6e7bfe3e8a38d0bf
katherinevfry <katyvfry@gmail.com>
2021-12-06 21:15:47 -0600

    categoriesFonts endpoints

--- JustMyType/DataAccess/CategoryFontsRepository.cs
+++ JustMyType/DataAccess/CategoryFontsRepository.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Dapper;
+using JustMyType.Models;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +15,38 @@ namespace JustMyType.DataAccess
         public CategoryFontsRepository(IConfiguration config)
         {
             _connectionString = config.GetConnectionString("JustMyType");
+        }
+
+        public IEnumerable<CategoriesFonts> GetCategoriesFontsByCategoryId(Guid id)
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var sqlString = @"select * from categoriesFonts where categoryId = @id";
+
+            var userCategoriesFonts = db.Query<CategoriesFonts>(sqlString, new { id });
+
+            return userCategoriesFonts;
+        }
+
+        internal void Add(CategoriesFonts newCategoryFont)
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var sqlString = @"insert into categoriesFonts(fontId, categoryId)
+                                                output inserted.id
+                                                values(@fontId, @categoryId)";
+            var id = db.ExecuteScalar<Guid>(sqlString, newCategoryFont);
+            newCategoryFont.Id = id;
+
+        }
+
+        internal void Remove(Guid id)
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var sqlString = @"delete from categoriesFonts where id = @id";
+
+            db.Execute(sqlString, new { id });
         }
     }
 }
